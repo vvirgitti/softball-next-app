fb56a76e6ff884dae3a2ab705ce05a93b6656360
Vanessa Virgitti <virgitti@gmail.com>
2020-01-16 14:19:08 +0000

    Can now add a new player and it is displayed on the page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,60 @@
-import React from 'react'
+import React, { useState, FormEvent } from 'react'
 import Head from 'next/head'
 
-const Index = () => (
-  <>
-    <Head>
-      <title>Softball lineup app</title>
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous" />
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossOrigin="anonymous"></script>
-    </Head>
-    <nav className="navbar navbar-light bg-light">
-      <span className="navbar-brand mb-0 h1">Lineup Builder</span>
-    </nav>
-    <h1>Softball app</h1>
-  </>
-)
+const Index: React.FC = () => {
+  const [playerName, setPlayerName] = useState('')
+  const [playersList, setPlayersList] = useState([playerName])
+
+  const handleNameChange = (event: { target: HTMLInputElement }) => {
+    setPlayerName(event.target.value)
+  }
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    setPlayersList([...playersList, playerName])
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Softball lineup app</title>
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossOrigin="anonymous"></script>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous" />
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossOrigin="anonymous"></script>
+      </Head>
+      <nav className="navbar navbar-light bg-light">
+        <span className="navbar-brand mb-0 h1">Lineup Builder</span>
+      </nav>
+      <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          <h2>Add a new player</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label>Name</label>
+              <input 
+                type="text" 
+                className="form-control" 
+                id="playerName" 
+                aria-describedby="playerName" 
+                data-testid="nameInput"
+                placeholder="Enter name"
+                onChange={handleNameChange} 
+                value={playerName}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary float-right">Add player</button>
+          </form>
+        </div>
+        <div className="col">
+          <h2>Roster</h2>
+            {playersList.length > 1 ? <ul>{playersList.map(player => <li key={player}>{player}</li>)}</ul> : <></>}
+        </div>
+
+      </div>
+    </div>
+    </>
+  )
+}
 
 export default Index
---
+++ tests/unit/pages/index.spec.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { render, getNodeText, fireEvent } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import '@testing-library/jest-dom/extend-expect'
+import Index from '../../../pages/index'
+
+describe('Index page', () => {
+  it('should display the text text in the nav bar', () => {
+    const { getByText } = render(<Index />)
+    const text = getByText('Lineup Builder')
+    expect(getNodeText(text)).toEqual('Lineup Builder')
+  })
+
+  it('should add a new player and display the name', () => {
+    const { getByText, getByTestId } = render(<Index />)
+
+    userEvent.type(getByTestId('nameInput'), 'Orla')
+    expect(getByTestId('nameInput')).toHaveAttribute('value', 'Orla')
+
+    const button = getByText('Add player')
+
+    fireEvent.click(button)
+
+    const name = getByText('Orla')
+    expect(getNodeText(name)).toEqual('Orla')
+  })
+})
