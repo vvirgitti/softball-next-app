aa7c18957cbcb9b9d56f29516a1cb0a9061dfb4c
Vanessa Virgitti <virgitti@gmail.com>
2020-01-16 11:15:14 +0000

    Added test for index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,7 @@
 import React from 'react'
-import Link from 'next/link'
 
-export default () => (
-  <ul>
-    <li>
-      <Link href="/a" as="/a">
-        <a>a</a>
-      </Link>
-    </li>
-    <li>
-      <Link href="/b" as="/b">
-        <a>b</a>
-      </Link>
-    </li>
-  </ul>
+const Index = () => (
+  <h1>Softball app</h1>
 )
+
+export default Index
