3a19e30869f89dddc92c64822a82a539ec9b3e8b
Vanessa Virgitti <virgitti@gmail.com>
2020-01-30 10:56:12 +0000

    Extracted the header to its own file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Head from 'next/head'
+import Header from '../components/Header'
 import PlayersTable from '../components/PlayersTable'
 
 type FormElem = React.FormEvent<HTMLFormElement>;
@@ -28,12 +28,7 @@ const App: React.FC = () => {
 
   return (
     <>
-      <Head>
-        <title>Softball lineup app</title>
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossOrigin="anonymous"></script>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossOrigin="anonymous" />
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossOrigin="anonymous"></script>
-      </Head>
+      <Header />
       <nav className="navbar navbar-light bg-light">
         <span className="navbar-brand mb-0 h1">Lineup Builder</span>
       </nav>
